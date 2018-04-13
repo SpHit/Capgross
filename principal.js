f39597f7f287388b11e80983395da0b39088edfe
Eric <eric@gmail.com>
2018-04-13 15:33:37 +0200

    redimension img personajes

--- principal.js
+++ principal.js
@@ -6,8 +6,12 @@ spritesheet = animacion
 frame = elige img en concreto
 */
 var personaje;
+var personaje2;
 var pelota;
 var cursor;
+var derecha;
+var izquierda;
+var arriba;
 var puntos1;
 var puntos2;
 var txtPuntos1;
@@ -20,53 +24,55 @@ var estado_princ = {
     
     preload: function(){
         capgross.load.image('fondo', 'img/campo.png');
-        capgross.load.image('jugador1', 'img/alex.png');
-        capgross.load.image('pelota', 'img/pelota2.png');
+        capgross.load.image('jugador1', 'img/EricR.png');
+        capgross.load.image('pelota', 'img/Pelota2.png');
+        capgross.load.image('jugador2', 'img/JordiR.png');
     },
     
 
     create: function(){
         capgross.add.tileSprite(0, 0, 1074, 724, 'fondo');
         personaje = capgross.add.sprite(capgross.width, capgross.height, 'jugador1');
-        pelota = capgross.add.sprite(capgross.width/2, capgross.height/2, 'pelota');
+        personaje2 = capgross.add.sprite(capgross.width/2, capgross.height/2, 'jugador2');
         
-
         ball = create_ball(capgross.world.centerX,capgross.world.centerY);
 
 
         capgross.input.onDown.add(launch_ball, this);
         
 
-
-
-        pelota.anchor.setTo(0.5,0.5);
-        pelota.scale.setTo(0.4,0.4);
-        personaje.anchor.setTo(1,1);     
         
-        personaje.scale.setTo(0.23,0.23);
+        personaje.anchor.setTo(1);
+        personaje.scale.setTo(1,1);
+        personaje2.anchor.setTo(-2.07);
+        personaje2.scale.setTo(-1, 1);
         
         ball_launched = false;
         ball_velocity = 400; 
 
 
 
-        capgross.physics.arcade.enable(personaje, pelota);
-        capgross.physics.arcade.enable(pelota);
+        capgross.physics.arcade.enable(personaje, ball);
+        capgross.physics.arcade.enable(personaje2, ball);
 
-        pelota.body.gravity.y = 700;
 
-        
-        
 
         cursor = capgross.input.keyboard.createCursorKeys();
+        derecha = capgross.input.keyboard.addKey(Phaser.Keyboard.D);
+        izquierda = capgross.input.keyboard.addKey(Phaser.Keyboard.A);
+        arriba = capgross.input.keyboard.addKey(Phaser.Keyboard.W);
+
         capgross.physics.startSystem(Phaser.Physics.ARCADE);
         
         
         personaje.body.gravity.y = 1900;
-        pelota.body.collideWorldBounds = true;
-        pelota.body.bounce.set();
+        personaje2.body.gravity.y = 1900;   
+        
+   
 
         personaje.body.collideWorldBounds = true;
+        personaje2.body.collideWorldBounds = true;
+
         puntos1 = 0;
         puntos2 = 0;
         txtPuntos1 = capgross.add.text(30, 20, "0", {font:"30px Arial", fill:"black"});
@@ -88,33 +94,44 @@ var estado_princ = {
             personaje.body.velocity.y = -600;
             
         }
+        if (derecha.isDown){
+            personaje2.position.x += 4;
+        }
+        if (izquierda.isDown){
+            personaje2.position.x -= 4;
+        }
+        if (arriba.isDown && personaje2.body.blocked.down){
+            personaje2.body.velocity.y = -600;
+        }
+        
 
-        capgross.physics.arcade.collide(personaje,ball);
-
-        if(pelota.body.blocked.left){
+        capgross.physics.arcade.collide(personaje, personaje2);
+        capgross.physics.arcade.collide(personaje, ball);
+        capgross.physics.arcade.collide(personaje2, ball);
+        
+        if(ball.body.blocked.left){
             console.log("A TOCADO EL BORDE")
         }
-        if(pelota.body.blocked.right){
+        if(ball.body.blocked.right){
             console.log("A TOCADO EL derecho")
         }
 
-
-
+        ball.angle += 1;
+        
         personaje.body.immovable=true;
+        personaje2.body.immovable=true;
 
-
-        pelota.angle += 1;
     }
 };
 function create_ball(x,y){
-    var ball = capgross.add.sprite(x,y,'img/pelota2.png');
+    var ball = capgross.add.sprite(x,y,'pelota');
     ball.anchor.setTo(0.5,0.5);
     capgross.physics.arcade.enable(ball);
     ball.body.collideWorldBounds = true;
     ball.body.bounce.setTo(0.58,0.58);
     ball.body.gravity.y = 700;
+    ball.scale.setTo(0.4,0.4);
     return ball;
-
 }
 function launch_ball(){
     if(ball_launched){
@@ -126,7 +143,6 @@ function launch_ball(){
         ball.body.velocity.x = ball_velocity;
         ball.body.velocity.y = ball_velocity;
         ball_launched = true;
-        
     }
 }
 
